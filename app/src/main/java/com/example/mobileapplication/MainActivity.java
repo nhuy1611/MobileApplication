617a369f9c1b8da1beca4db1788e495a50c95847
Nhu Y <nhuyld16@gmail.com>
2023-09-13 12:05:55 +0700

    ProgressBar

--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.mobileapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button showProgressBtn;
+    private ProgressBar loadingPB;
+    boolean isProgressVisible = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showProgressBtn = findViewById(R.id.idBtnDisplayProgress);
+        loadingPB = findViewById(R.id.idPBLoading);
+        showProgressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isProgressVisible){
+                    showProgressBtn.setText("Show Progress Bar");
+                    loadingPB.setVisibility(View.GONE);
+                    isProgressVisible = false;
+                } else {
+                    isProgressVisible = true;
+                    showProgressBtn.setText("Hide Progress Bar");
+                    loadingPB.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
+

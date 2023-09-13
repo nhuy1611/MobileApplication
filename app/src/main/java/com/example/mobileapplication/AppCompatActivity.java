30efdb45104be9eb8276e8f6d4206a0d63f1a546
Nhu Y <nhuyld16@gmail.com>
2023-09-13 12:13:12 +0700

    Seekbar

---
+++ app/src/main/java/com/example/mobileapplication/AppCompatActivity.java
@@ -0,0 +1,7 @@
+package com.example.mobileapplication;
+
+import android.os.Bundle;
+
+public abstract class AppCompatActivity {
+    protected abstract void onCreate(Bundle savedInstanceState);
+}
--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.mobileapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    SeekBar simpleSeekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simpleSeekBar=(SeekBar)findViewById(R.id.simpleSeekBar);
+        // perform seek bar change listener event used for getting the progress value
+        simpleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }

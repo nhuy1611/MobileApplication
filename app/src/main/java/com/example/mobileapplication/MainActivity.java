ed76f160ba5d6fc4d98f5eb6b0ff30f4a0b558e6
Nhu Y <nhuyld16@gmail.com>
2023-09-13 14:06:40 +0700

    textView

--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mobileapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Lấy TextView trong Java
+        TextView mytextview;
+        mytextview = findViewById(R.id.text_view);
+
     }
 }

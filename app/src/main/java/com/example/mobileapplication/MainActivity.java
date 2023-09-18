abe69676702e25d6271072983f3eaa1108c41006
Nhu Y <nhuyld16@gmail.com>
2023-09-18 13:33:55 +0700

    update

--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -1,29 +1,27 @@
 package com.example.mobileapplication;
 
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
-
 public class MainActivity extends AppCompatActivity {
-
     ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         assert binding != null;
         setContentView(binding.getRoot());
-
         replaceFragment(new HomeFragment());
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            int id = item.getItemId();
+
+        binding.bottomNavigationView.setOnItemSelectedListener(menuItem -> {
+            int id = menuItem.getItemId();
             if (id == R.id.home) {
                 replaceFragment(new HomeFragment());
             } else if (id == R.id.user) {
@@ -33,9 +31,10 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+
     }
 
-    private void replaceFragment(Fragment fragment) {
+    private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);

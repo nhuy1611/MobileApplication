160b3871ce913f4ec4645c0d37559c0f5fe80424
Nhu Y <nhuyld16@gmail.com>
2023-09-20 00:04:31 +0700

    ....

--- app/src/main/java/com/example/mobileapplication/DataAdapter.java
+++ app/src/main/java/com/example/mobileapplication/DataAdapter.java
@@ -1,53 +1,53 @@
-package com.example.mobileapplication;
-
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
-
-public class DataAdapter extends BaseAdapter {
-
-    private final String[] items;
-    private final Activity activity;
-
-    public DataAdapter(String[] items, Activity activity) {
-        this.items = items;
-        this.activity = activity;
-    }
-
-    @Override
-    public int getCount() {
-        return items.length;
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return items[i];
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    @SuppressLint({"InflateParams", "ViewHolder"})
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        // Gọi layoutInflater ra để bắt đầu ánh xạ view và data.
-
-        LayoutInflater inflater = activity.getLayoutInflater();
-
-        // Đổ dữ liệu vào biến View, view này chính là những gì nằm trong item_name.xml
-        view = inflater.inflate(R.layout.item_name, null);
-
-        // Đặt chữ cho từng view trong danh sách.
-        TextView tvName = (TextView) view.findViewById(R.id.tv_name);
-        tvName.setText(items[i]);
-
-        // Trả về view kết quả.
-        return view;
-    }
-}
+//package com.example.mobileapplication;
+//
+//import android.annotation.SuppressLint;
+//import android.app.Activity;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.TextView;
+//
+//public class DataAdapter extends BaseAdapter {
+//
+//    private final String[] items;
+//    private final Activity activity;
+//
+//    public DataAdapter(String[] items, Activity activity) {
+//        this.items = items;
+//        this.activity = activity;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return items.length;
+//    }
+//
+//    @Override
+//    public Object getItem(int i) {
+//        return items[i];
+//    }
+//
+//    @Override
+//    public long getItemId(int i) {
+//        return i;
+//    }
+//
+//    @SuppressLint({"InflateParams", "ViewHolder"})
+//    @Override
+//    public View getView(int i, View view, ViewGroup viewGroup) {
+//        // Gọi layoutInflater ra để bắt đầu ánh xạ view và data.
+//
+//        LayoutInflater inflater = activity.getLayoutInflater();
+//
+//        // Đổ dữ liệu vào biến View, view này chính là những gì nằm trong item_name.xml
+//        view = inflater.inflate(R.layout.item_name, null);
+//
+//        // Đặt chữ cho từng view trong danh sách.
+//        TextView tvName = (TextView) view.findViewById(R.id.tv_name);
+//        tvName.setText(items[i]);
+//
+//        // Trả về view kết quả.
+//        return view;
+//    }
+//}
--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.mobileapplication;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    String[] items = {"Nguyen Van A", "Le Thi B", "Phan Minh C"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] items = {"Nguyen Van A", "Le Thi 8", "Phan Minh C"};
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        DataAdapter adapter = new DataAdapter(items, null);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.item_name, items);
+
+        ListView listView = (ListView) findViewById(R.id.course_list);
         listView.setAdapter(adapter);
     }
 }
+
+
+
+
+

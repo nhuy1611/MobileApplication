9dbd742fa97f5ed83303e9878bdd3d701cb7e218
Nhu Y <nhuyld16@gmail.com>
2023-09-19 22:52:38 +0700

    ....

--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mobileapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] items = {"Nguyen Van A", "Le Thi 8", "Phan Minh C"};
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        DataAdapter adapter = new DataAdapter(items, null);
+        listView.setAdapter(adapter);
     }
 }

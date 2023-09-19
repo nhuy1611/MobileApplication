b69af28a1ecd45f1a26d1c42b5bd6070d71a65df
Nhu Y <nhuyld16@gmail.com>
2023-09-19 14:47:58 +0700

    Switch

--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.mobileapplication;
 
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Switch switchView;
+    private TextView statusTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchView = findViewById(R.id.idSwitch);
+        statusTV = findViewById(R.id.idTVStatus);
+
+        if (switchView.isChecked()) {
+            statusTV.setText("Switch is Checked");
+        } else {
+            statusTV.setText("Switch is UnChecked");
+        }
+
+        switchView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    statusTV.setText("Switch is Checked");
+                } else {
+                    statusTV.setText("Switch is UnChecked");
+                }
+            }
+        });
     }
 }
